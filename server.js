0c17b2df4d6c32d3c0520b20d4bc425aa2edb947
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2018-02-27 11:46:41 +0100

    Don't overwrite pre-existing values on initialization of etcd paths

--- server.js
+++ server.js
@@ -63,7 +63,9 @@ app.get('/metrics', (req, res) => {
 });
 
 REQUIRED_SERVICES.forEach(function (serviceName) {
-  etcd.set(`/ruhmesmeile/projects/typo3/${STAGE}/${PROJECTKEY}/status/${serviceName}/current`, 'stopped', function (err, res) {
+  etcd.set(`/ruhmesmeile/projects/typo3/${STAGE}/${PROJECTKEY}/status/${serviceName}/current`, 'stopped', {
+    prevExist: false
+  }, function (err, res) {
     err ? console.log(err) : etcd.set(`/ruhmesmeile/projects/typo3/${STAGE}/${PROJECTKEY}/status/${serviceName}/stopped`, (new Date()/1000), function (err, res) {
       if (err) console.log(err);
     });
